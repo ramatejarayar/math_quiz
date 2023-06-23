421ace9b687daf62e8d41877532e75e4988f0881
ramatejarayar <122608308+ramatejarayar@users.noreply.github.com>
2023-06-23 16:24:08 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,16 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+
+	  //Set "player2_name" using localStorage.setItem() function
+
+    localStorage.setItem("name1", player1_name)
+
+    localStorage.setItem("name2", player2_name)
+
+    window.location = "game_page.html";
+}
+
